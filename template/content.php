d9ecd9c01dfe621174ef48fe129f5015b2397f3d
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-20 20:28:37 +0700

    add file content

---
+++ template/content.php
@@ -0,0 +1,12 @@
+<div class="col-md-12 container">
+    <?php
+        $module = $_GET["module"];
+        if($module = "detail_film"){
+
+        }elseif($module = "profile"){
+
+        }else{
+            require_once 'default.php';
+        }
+    ?>
+</div>
