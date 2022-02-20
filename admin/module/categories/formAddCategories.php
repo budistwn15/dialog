05f2b8d318b48d77491208b1cd097c85ab41f9aa
budistwn15 <budi.setiawan112001@gmail.com>
2022-02-20 20:23:11 +0700

    add file form add category

---
+++ admin/module/categories/formAddCategories.php
@@ -0,0 +1,15 @@
+<div class="box-form">
+	<div class="header"></div>
+	<form action="module/categories/addCategories.php" method="POST">
+	<div class="form-group" style="margin-top: 0px;">
+		<label>Name</label>
+		<br>
+		<input type="text" name="name">
+	</div>
+	<br>
+	<div class="form-group">
+		<button type="submit" class="btn-save">Simpan</button>
+	</div>
+	</form>
+	<div class="footer"></div>
+</div>
